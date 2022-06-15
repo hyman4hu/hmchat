488538ec9b41b0c3be79fb52b83cfb971a69ef8d
Hyman <hyman4hu@qq.com>
2022-06-15 20:00:28 +0800

    qqq

---
+++ src/main/java/fun/hyman4hu/hmchat/controller/SysAttachmentController.java
@@ -0,0 +1,21 @@
+package fun.hyman4hu.hmchat.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 附件
+ * 
+ * @author Hyman
+ * @date 2022/06/15
+ */
+@Slf4j
+@RestController
+@RequestMapping("/attachment")
+@RequiredArgsConstructor
+public class SysAttachmentController {
+
+}
